b4bb43d8b7846979586f3be92f1254b64ad1fdc0
galacticor <31152419+galacticor@users.noreply.github.com>
2019-10-17 11:25:00 +0700

    Update TP2-DDP1.py

--- TP2-DDP1.py
+++ TP2-DDP1.py
@@ -8,6 +8,7 @@ lst_freq = []
 stopwords = []
 lst_final = []
 
+# Input stopwords yaitu kata-kata yang tidak boleh ada atau diabaikan
 def input_stopwords():
 	File = open("stopWords.txt", "r")
 	stopwords = []
@@ -16,6 +17,7 @@ def input_stopwords():
 	File.close()
 	return stopwords
 
+# Input kata-kata dalam file text
 def input_data(file):
 	teks = ''
 	for baris in file:
@@ -23,6 +25,7 @@ def input_data(file):
 	teks = teks.split()
 	return teks
 
+# Buang tanda baca atau punctuation yang seharusnya diabaikan
 def buang_tanda(teks):
 	ret_teks = []
 	kata_baru = ''
@@ -33,6 +36,7 @@ def buang_tanda(teks):
 		ret_teks.append(kata_baru.lower())
 	return ret_teks
 
+# Menghitung frequensi kemunculan kata untuk setiap kata di dalam teks
 def hitung_freq(teks):
 	for kata in teks:
 		if len(kata) > 0 and kata not in stopwords:
@@ -47,6 +51,7 @@ def hitung_freq(teks):
 			else:
 				lst_freq[idx] += 1
 
+# Fungsi untuk mencetak tabel max 36 list of words
 def print1():
         lebar_freq = max(len(str(data[0])) for data in lst_final) + 1
         lebar_kata = max(len(data[1]) for data in lst_final) + 16
@@ -65,6 +70,9 @@ def print1():
 print("Create word frequency table and bar chart from a text file")
 print("----------------------------------------------------------")
 file_name = input("Please enter the file name: ")
+#   JokoWidodoSpeechAPEC2014.txt
+#   CommencementSpeechByGates2014.txt
+
 
 File = open(file_name,'r')
 stopwords = input_stopwords()
@@ -73,17 +81,36 @@ teks = buang_tanda(teks)
 hitung_freq(teks)
 File.close()
 
+# Masukkan ke dalam tuple agar bisa disort
 for i in range(len(lst_kata)):
     lst_final.append((lst_freq[i],lst_kata[i]))
 
+# Sort secara terbalik berdasarkan index 1 atau frequensi
 lst_final.sort(reverse = True)
 if len(lst_final)>36:
     lst_final = lst_final[:36]
 print1()
+# Sort secara terbalik berdasarkan index 2 atau abjad
+lst_final.sort(key = lambda x : x[1],reverse = True)
+
+lst_kata = []
+lst_freq = []
+# Copy dari list of tuple ke dalam list untuk dicetak dalam matplotlib
+for i in range(len(lst_final)):
+	lst_kata.append(lst_final[i][1])
+	lst_freq.append(lst_final[i][0])
 
 lebar = 0.75
+# bikin list arrange
 banyakBar = np.arange(len(lst_kata))
+# set bar untuk horizontal
 plt.barh(banyakBar, lst_freq,lebar)
+# atur label y axis 
 plt.yticks(banyakBar, lst_kata)
-
+# atur nama label x
+plt.xlabel('Frequency')
+# atur judul
+plt.title('Top 36 Words in Alphabetical Order')
+# show the figure
 plt.show()
+input ("Please type Enter to exit ...")
