76b6b73200277cd8aa734ab6cba1b47010b0e0f4
galacticor <31152419+galacticor@users.noreply.github.com>
2019-11-07 12:03:43 +0700

    Create Lab07.py

---
+++ Lab07.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+def recrev(st):
+	if(len(st)==0):
+		return ""
+	a = st[-1]
+	st = st[:-1]
+	return a+recrev(st)
+
+@app.route("/")
+@app.route("/<word>")
+def main(word='fasilkom'):
+	page = '''
+<html>
+	<head> <title>Lab 07 Page</title> <head>
+	<body>
+		<h1 style="color: red">Lab 07 Dasar-Dasar Pemorgraman 1</h1>
+		<p>The reverse of <i style="color: green">{{kata}}</i> is
+		<b style="color: blue">{{kebalik}}</b>.
+	</body>
+</html>
+'''
+	return render_template_string(page,kata=word,kebalik=recrev(word))
+    
+if __name__ == "__main__":
+    app.run(debug=True)
