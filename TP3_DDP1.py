f51920643b07a99aee86904da34e07c3554c01ba
galacticor <31152419+galacticor@users.noreply.github.com>
2019-11-13 20:20:38 +0700

    Update TP3_DDP1.py

--- TP3_DDP1.py
+++ TP3_DDP1.py
@@ -2,24 +2,28 @@ semua = {}
 
 def impor(query):
 	query = query.replace('IMPOR ','').strip()
-	file = open(query,'r')
-	count = 0;
-	for baris in file:
-		baris = baris[1:-2]
-		tmp = baris.split(',')
-		semua[tmp[0]] = tmp[:]
-		count += 1
-	print("Terimpor {} baris".format(count))
-	print(semua)
-
+	try:
+		file = open(query,'r')
+		count = 0;
+		for baris in file:
+			baris = baris[1:-1]
+			tmp = baris.split(',')
+			semua[tmp[0]] = tmp[:]
+			count += 1
+		print("Terimpor {} baris".format(count))
+	except:
+		print('File tidak ada')
+	
 def ekspor(query):
 	query = query.replace('EKSPOR ','').strip()
 	######
 
 def cari_nama(query):
 	query = query.replace('CARINAMA ','').strip()
-	tmp = semua[query]
-	print("{}, {}, {}, {}".format(tmp[0],tmp[1],tmp[2],tmp[3]))
+	if query in semua:
+		tmp = semua[query]
+		print("{}, {}, {}, {}".format(tmp[0],tmp[1],tmp[2],tmp[3]))
+	else: print("{} tidak ditemukan".format(query))
 
 def cari_tipe(query):
 	query = query.replace('CARITIPE ','').strip()
@@ -29,7 +33,8 @@ def cari_tipe(query):
 			count += 1
 			tmp = semua[data]
 			print("{}, {}, {}, {}".format(tmp[0],tmp[1],tmp[2],tmp[3]))
-	print("*Ditemukan {} {}*".format(count,query))
+	if count!=0 : print("*Ditemukan {} {}*".format(count,query))
+	else: print("{} tidak ditemukan".format(query))
 
 def cari_prov(query):
 	query = query.replace('CARIPROV ','').strip()
@@ -39,29 +44,30 @@ def cari_prov(query):
 			count += 1
 			tmp = semua[data]
 			print("{}, {}, {}, {}".format(tmp[0],tmp[1],tmp[2],tmp[3]))
-	print("*Ditemukan {} warisan budaya*".format(count,query))
+	if count!=0 : print("*Ditemukan {} {}*".format(count,query))
+	else: print("{} tidak ditemukan".format(query))
 
 def tambah(query):
 	query = query.replace('TAMBAH ','').strip()
-	query = query[1:]
-	query = query.split(',,,')
+	query = query.split(';;;')
 	if query[0] in semua:
-		print('Data telah ada')
+		print('{} telah ada'.format(query[0]))
 	else:
 		semua[query[0]] = query[:]
 
 def update(query):
 	query = query.replace('UPDATE ','')
-	query = query[1:]
-	query = query.split(',,,')
-	semua[query[0]] = query[:]
+	query = query.split(';;;')
+	if query[0] not in semua:
+		print('{} belum ada'.format(query[0]))
+	else:
+		semua[query[0]] = query[:]
 
 def hapus(query):
  	query = query.replace('HAPUS ','').strip()
- 	query = query[1:]
  	try:
  		del semua[query]
- 		print("{} dihapus".format(query))
+ 		print("{} telah dihapus".format(query))
  	except:
  		print("{} memang tidak ada".format(query))
 
@@ -69,30 +75,49 @@ def stat():
  	print("Terdapat {} warisan budaya".format(len(semua)))
 
 def stat_tipe():
- 	for data in semua:
- 		semua[data]
+	dict_tipe = {}
+	tipe = []
+	for data in semua:
+		if semua[data][1] not in dict_tipe:
+			dict_tipe[semua[data][1]] = 0
+		dict_tipe[semua[data][1]] += 1
+	for k,v in dict_tipe.items():
+		tipe.append((k,v))
+	tipe.sort(key = lambda x: x[1], reverse = True)
+	print(tipe)
 
+def stat_prov():
+	dict_prov = {}
+	prov = []
+	for data in semua:
+		if semua[data][2] not in dict_prov:
+			dict_prov[semua[data][2]] = 0
+		dict_prov[semua[data][2]] += 1
+	for k,v in dict_prov.items():
+		prov.append((k,v))
+	prov.sort(key = lambda x: x[1], reverse = True)
+	print(prov)
 
-
-def main(): 
+def main():
 	while(True):
-		query = input().strip()
-		if 'IMPOR' in query:
-			impor(query)
-		elif 'EKSPOR' in query:
-			ekspor(query)
-		elif 'CARINAMA' in query:
-			cari_nama(query)
-		elif 'CARITIPE' in query:
-			cari_tipe(query)
-		elif 'CARIPROV' in query:
-			cari_prov(query)
-		elif 'TAMBAH' in query:
-			tambah(query)
-		elif 'UPDATE' in query:
-			update(query)
-		elif 'HAPUS' in query:
-			hapus(query)
+		print("#"*5)
+		query = input("Masukkan perintah : ").strip()
+		if 'IMPOR' in query: impor(query)
+		elif 'EKSPOR' in query: ekspor(query)
+		elif 'CARINAMA' in query: cari_nama(query)
+		elif 'CARITIPE' in query: cari_tipe(query)
+		elif 'CARIPROV' in query: cari_prov(query)
+		elif 'TAMBAH' in query: tambah(query)
+		elif 'UPDATE' in query: update(query)
+		elif 'HAPUS' in query: hapus(query)
+		elif 'STATTIPE' in query: stat_tipe()
+		elif 'STATPROV' in query: stat_prov()
+		elif 'STAT' in query: stat()
+		elif 'cetak' in query:
+			print (semua)
 
 if __name__ == "__main__":
-        main()
+	print("#"*5)
+	print("BudayaKB Lite v1.0")
+	print("~Kalau bukan kita yang melestarikan budaya, siapa lagi?~")
+	main()
